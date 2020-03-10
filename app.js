18734e756602bbf0dd7beffb60c2968cc402bc2f
jferson1089 <59752992+jferson1089@users.noreply.github.com>
2020-03-10 00:20:48 -0400

    html, css, and app.js, html layout and css styling

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log('JS is up and running')
+
+
+let url = 'https://docs.google.com/spreadsheets/d/11VTYvZzzxHV_I867DbtkGCGE9T6Ld8V5kEhqJI_fVQI/edit#gid=0'
+let id = '11VTYvZzzxHV_I867DbtkGCGE9T6Ld8V5kEhqJI_fVQI'
+
+let source =`https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+
+fetch(source)
+  .then( response => response.json() ) //this converts date from string back into an object
+  .then( data => {
+  console.log('data', data)//this provides us access to the parsed data
+  let projects = data.feed.entry.map(project => {
+    // console.log('project', project.gsx$title.$t)
+    return {
+      title: project.gsx$title.$t,
+      image: project.gsx$image.$t,
+      description:project.gsx$description.$t,
+      url:project.gsx$url.$t
+      
+    }
+  })
+  app(projects)
+})
+
+function app (projects){
+    console.log('app-projects', projects)
+}
+
+let addProject = ( ) => {
+    image = projects.image
+    let $img = $('<img>')
+    let $div = $('<div>').setClass()
+    
+}
