35252dd382b2d05db6dc001338770eecce96815d
jferson1089 <59752992+jferson1089@users.noreply.github.com>
2020-03-11 10:59:18 -0400

    fixed styling, hamburger menu added

--- app.js
+++ app.js
@@ -33,3 +33,10 @@ let addProject = ( ) => {
     let $div = $('<div>').setClass()
     
 }
+
+//This section is for the click function on hamburger icon
+$('.menu-toggle').click(function() {
+  
+    $('.site-nav').toggleClass('site-nav--open', 400)
+    $(this).toggleClass('open')
+})
