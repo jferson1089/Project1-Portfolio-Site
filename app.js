6c2e01e5e907c56a9e8072c9b4beda2b0a86512f
jferson1089 <59752992+jferson1089@users.noreply.github.com>
2020-03-16 09:51:36 -0400

    formatting small changes

--- app.js
+++ app.js
@@ -4,91 +4,91 @@ console.log('JS is up and running')
 let url = 'https://docs.google.com/spreadsheets/d/11VTYvZzzxHV_I867DbtkGCGE9T6Ld8V5kEhqJI_fVQI/edit#gid=0'
 let id = '11VTYvZzzxHV_I867DbtkGCGE9T6Ld8V5kEhqJI_fVQI'
 
-let source =`https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+let source = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
 
 fetch(source)
-  .then( response => response.json() ) //this converts date from string back into an object
-  .then( data => {
-  console.log('data', data)//this provides us access to the parsed data
-  let projects = data.feed.entry.map(project => {
-    // console.log('project', project.gsx$title.$t)
-    return {
-      title: project.gsx$title.$t,
-      image: project.gsx$image.$t,
-      description:project.gsx$description.$t,
-      url:project.gsx$url.$t
-      
-    }
-  })
-  app(projects)
-  showSlides()
+    .then(response => response.json()) //this converts date from string back into an object
+    .then(data => {
+        console.log('data', data)//this provides us access to the parsed data
+        let projects = data.feed.entry.map(project => {
+            // console.log('project', project.gsx$title.$t)
+            return {
+                title: project.gsx$title.$t,
+                image: project.gsx$image.$t,
+                description: project.gsx$description.$t,
+                url: project.gsx$url.$t
 
-  //This section is for the click function on hamburger icon. resources linked on style sheet.
-$('.menu-toggle').click(function() {
-  
-    $('.site-nav').toggleClass('site-nav--open', 400)
-    $(this).toggleClass('open')
-})
+            }
+        })
+        app(projects)
+        showSlides()
 
+        //This section is for the click function on hamburger icon. resources linked on style sheet.
+        $('.menu-toggle').click(function () {
 
+            $('.site-nav').toggleClass('site-nav--open', 400)
+            $(this).toggleClass('open')
+        })
 
 
 
-})
 
-function app (projects){
+
+    })
+
+function app(projects) {
     console.log('app-projects', projects)
 
-    
 
-    projects.forEach( container => {
+
+    projects.forEach(container => {
         let $li = $(`<div class="imageContainer"><h2 class='title'>${container.title}</h2><a href='${container.url}'><img src='${container.image}' class='containerImage' style="width:150px;height:100px;"></img></a><p>${container.description}</p></div>`)
-      
+
         $('.section1').append($li);
 
 
 
 
-        
-        let $slide=$(`<div class=slideBox fade>
+
+        let $slide = $(`<div class=slideBox fade>
 <a href='${container.url}'>
 <img src='${container.image}' style ='width: 500px; height:500px;'></a>
 <div>${container.title} - ${container.description} </div>`)
 
-$('div.slideshow-container').append($slide)
+        $('div.slideshow-container').append($slide)
 
-      })
-      
-
+    })
 
 
 
 
 
-    }  
 
-    var slideIndex = 0;
-    showSlides();
-    
-    function showSlides() {
-      let i;
-      let slides = document.getElementsByClassName("slideBox");
-      for (i = 0; i < slides.length; i++) {
+
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slideBox");
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}
-      slides[slideIndex-1].style.display = "block";
-      setTimeout(showSlides, 5000); // Change image every 2 seconds
     }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
 
 
 
 
 
 //This section is for the click function on hamburger icon. resources linked on style sheet.
-$('.menu-toggle').click(function() {
-  
+$('.menu-toggle').click(function () {
+
     $('.site-nav').toggleClass('site-nav--open', 400)
     $(this).toggleClass('open')
 })
